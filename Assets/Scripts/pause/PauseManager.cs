4b29a5b1f50ca5461e1d9c4c27ff10cef1b91afa
RafaCastrepy <16ccasraf@gmail.com>
2026-03-21 01:06:26 +0000

    pause added

---
+++ Assets/Scripts/pause/PauseManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    [SerializeField] GameObject pauseOverlay;
+
+    public void TogglePause()
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        pauseOverlay.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseOverlay.SetActive(false);
+    }
+}
