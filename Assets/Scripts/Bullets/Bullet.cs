6ec72ca357a3b39c4303bd2dcb5ecc298869f9ff
RafaCastrepy <16ccasraf@gmail.com>
2026-03-23 20:00:44 +0000

    bullets die when hitting boundary

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -97,5 +97,10 @@ public class Bullet : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+         if (other.CompareTag("border"))
+    {
+        Destroy(gameObject);
     }
+    }
+    
 }
